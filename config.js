c5aa1c78a8b82a4990bfa7ce72c4e590266b3b51
VIVEK DHIMAN <53940939+DHIMANvivek@users.noreply.github.com>
2023-04-27 13:57:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+/*
+create and export configuration variables
+*/
+
+// container for all the environments
+var environments = {};
+
+// staging (default) environment 
+environments.staging = {
+    'port' : 3000,
+    'envName' : 'staging'
+};
+
+// Production Environment
+environments.production = {
+    'port' : 5000,
+    'envName' : 'production'
+};
+
+// Determine which environment was passed as an command line argument
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// check that the current environment is one of the above , if not , default to staging
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// export the module
+module.exports = environmentToExport;
